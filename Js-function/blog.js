b7a50bbcade51cb6db2421ea09d058473fae07ca
Nipa <moninipa027@gmail.com>
2024-09-23 12:15:24 +0600

    first commit

---
+++ Js-function/blog.js
@@ -0,0 +1,9 @@
+// go to blog page
+document.getElementById('blog-btn').addEventListener('click',function(){
+    window.location.href ='./blog.html'
+    
+})
+// go to home page
+function goBack() {
+    window.history.back();
+}
