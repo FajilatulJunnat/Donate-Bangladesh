5b3a30236769784fa8d9b434e5bd70f70f7ac354
Nipa <moninipa027@gmail.com>
2024-09-23 20:17:27 +0600

    4th

--- Js-function/donate.js
+++ Js-function/donate.js
@@ -25,6 +25,15 @@ document.getElementById('donate-now1').addEventListener('click',function(){
             const navAmount=GetTextFieldById('nav-amount')
             const updateNavAmount =navAmount - addDonate;
             document.getElementById('nav-amount').innerText=updateNavAmount;
+
+            // tranjection history
+            const heading=GetHeading('head-1')
+     
+    const p= document.createElement('p')
+    p.innerText =`
+    ${addDonate} Taka is doneted for ${heading}
+    `
+    document.getElementById('history-container').appendChild(p)
             
         })
     }
@@ -36,26 +45,26 @@ document.getElementById('donate-now2').addEventListener('click',function(){
     const availableDonate2 =GetTextFieldById('total-donate2')
  
     // check validation
-    // if (isNaN(addDonate2)) {
-    //     alert('Invalid Donation Amount')
-    //     return
-    // }else if (addDonate2<0) {
-    //     alert('Invalid Donation Amount')
-    //     return
-    // }
-    // else{
-    //     document.getElementById("my_modal_2").showModal();
+    if (isNaN(addDonate2)) {
+        alert('Invalid Donation Amount')
+        return
+    }else if (addDonate2<0) {
+        alert('Invalid Donation Amount')
+        return
+    }
+    else{
+        document.getElementById("my_modal_2").showModal();
 
-    //     document.getElementById('close2').addEventListener('click',function(){
-    //         const updateAmount2=addDonate2 + availableDonate2;
-    //         document.getElementById("total-donate2").innerText=updateAmount2
+        document.getElementById('close2').addEventListener('click',function(){
+            const updateAmount2=addDonate2 + availableDonate2;
+            document.getElementById("total-donate2").innerText=updateAmount2
         
-    //         const navAmount=GetTextFieldById('nav-amount')
-    //         const updateNavAmount =navAmount - addDonate2;
-    //         document.getElementById('nav-amount').innerText=updateNavAmount;
+            const navAmount=GetTextFieldById('nav-amount')
+            const updateNavAmount =navAmount - addDonate2;
+            document.getElementById('nav-amount').innerText=updateNavAmount;
             
-    //     })
-    // }
+        })
+    }
 })
 
 document.getElementById('donate-now3').addEventListener('click',function(){
@@ -86,37 +95,27 @@ document.getElementById('donate-now3').addEventListener('click',function(){
 })
 
 
+
+// common function
 // input field function
 function GetInputValueById(id) {
     const inputMoney=document.getElementById(id);
     const inputMoneyValue=inputMoney.value;
      const inputMoneyNumber=parseFloat(inputMoneyValue);
      inputMoney.value = '';
-     if (isNaN(inputMoneyNumber)) {
-        alert('Invalid Donation Amount')
-        return
-    }else if (inputMoneyNumber<0) {
-        alert('Invalid Donation Amount')
-        return
-    }
-else{
-    document.getElementById("my_modal_2").showModal();
-    document.getElementById('close3').addEventListener('click',function(){
-        const updateAmount3=addDonate3 + availableDonate3;
-        document.getElementById("total-donate3").innerText=updateAmount3
-    
-        const navAmount=GetTextFieldById('nav-amount')
-        const updateNavAmount =navAmount - addDonate3;
-        document.getElementById('nav-amount').innerText=updateNavAmount;
-        
-    })
-}
+return inputMoneyNumber
      
  }
-
  // inner text function
 function GetTextFieldById(id) {
     const textField= document.getElementById(id).innerText;
     const textConvertNumber=parseFloat(textField)
     return textConvertNumber;
+}
+
+
+// inner text for heading
+function GetHeading(id) {
+    const head=document.getElementById(id).innerText
+    return head
 }
